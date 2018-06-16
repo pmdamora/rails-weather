42dbf5b4c70bd8f0e482aca0499e7d2ab9b8338d
Paul D'Amora <pmdamora@gmail.com>
2018-06-15 23:53:10 -0400

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,16 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/test/' 
+  add_filter '/app/channels/'
+  add_filter '/app/jobs/'
+  add_filter '/app/mailers/'
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
